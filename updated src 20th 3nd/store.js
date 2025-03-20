2a4d97132781fcde1dc8046dfbfc4f8acf6272b4
Daniel <81682668+danielokpanachi@users.noreply.github.com>
2025-03-20 07:16:19 +0100

    Add files via upload

---
+++ updated src 20th 3nd/store.js
@@ -0,0 +1,28 @@
+// src/store.js
+import { reactive } from 'vue';
+
+export const store = reactive({
+  testOrganizations: [
+    { name: 'Test Org 1',  referenceNumber: '', date: '', Subject: '', recipientTitle: '', senderDepartment: '', recipientName:'' },
+    { name: 'Test Org 2',  referenceNumber: '', date: '', Subject: '', recipientTitle: '', senderDepartment: '', recipientName:'' },
+    { name: 'Test Org 3',  referenceNumber: '', date: '', Subject: '', recipientTitle: '', senderDepartment: '', recipientName:'' },
+    { name: 'Test Org 4',  referenceNumber: '', date: '', Subject: '', recipientTitle: '', senderDepartment: '', recipientName:'' },
+    { name: 'Test Org 5',  referenceNumber: '', date: '', Subject: '', recipientTitle: '', senderDepartment: '', recipientName:'' },
+    { name: 'Test Org 6',  referenceNumber: '', date: '', Subject: '', recipientTitle: '', senderDepartment: '', recipientName:'' },
+    { name: 'Test Org 7',  referenceNumber: '', date: '', Subject: '', recipientTitle: '', senderDepartment: '', recipientName:'' },
+    { name: 'Test Org 8',  referenceNumber: '', date: '', Subject: '', recipientTitle: '', senderDepartment: '', recipientName:'' },
+    { name: 'Test Org 9',  referenceNumber: '', date: '', Subject: '', recipientTitle: '', senderDepartment: '', recipientName:'' },
+    { name: 'Test Org 10', referenceNumber: '', date: '', Subject: '', recipientTitle: '', senderDepartment: '', recipientName:'' }
+    /*{ name: 'Test Org 11', referenceNumber: '', date: '', subject: '', recipientTitle: '', senderDepartment: '' },
+    { name: 'Test Org 12', referenceNumber: '', date: '', subject: '', recipientTitle: '', senderDepartment: '' },
+    { name: 'Test Org 13', referenceNumber: '', date: '', subject: '', recipientTitle: '', senderDepartment: '' },
+    { name: 'Test Org 14', referenceNumber: '', date: '', subject: '', recipientTitle: '', senderDepartment: '' },
+    { name: 'Test Org 15', referenceNumber: '', date: '', subject: '', recipientTitle: '', senderDepartment: '' },
+    { name: 'Test Org 16', referenceNumber: '', date: '', subject: '', recipientTitle: '', senderDepartment: '' },
+    { name: 'Test Org 17', referenceNumber: '', date: '', subject: '', recipientTitle: '', senderDepartment: '' },
+    { name: 'Test Org 18', referenceNumber: '', date: '', subject: '', recipientTitle: '', senderDepartment: '' },
+    { name: 'Test Org 19', referenceNumber: '', date: '', subject: '', recipientTitle: '', senderDepartment: '' },
+    { name: 'Test Org 20', referenceNumber: '', date: '', subject: '', recipientTitle: '', senderDepartment: '' },
+    { name: 'Test Org '  , referenceNumber: '', date: '', subject: '', recipientTitle: '', senderDepartment: '' }*/
+  ]
+});
