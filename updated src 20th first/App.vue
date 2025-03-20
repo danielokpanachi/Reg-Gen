f3cd519e2a3e0dd568b639baa35c2acce233f01f
Daniel <81682668+danielokpanachi@users.noreply.github.com>
2025-03-20 06:51:33 +0100

    Add files via upload

---
+++ updated src 20th first/App.vue
@@ -0,0 +1,35 @@
+<template>
+
+<div id="app">
+    <transition name="fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+ 
+  }
+</script>
+
+<style>
+#app {
+  font-family: Arial, sans-serif;
+  background-color: white;
+  height: 100vh;
+  width: 100vw;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+
+  .fade-enter-active, .fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+
+</style>
